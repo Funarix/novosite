af9449387f03f6c5a08140aceb42799a6f754742
Gabriel Morais <gabriel.gdw@hotmail.com>
2024-08-29 20:48:29 -0400

    alteração 29/08

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('login/', views.login, name= 'login'), 
     path('cadastro/', views.cadastro, name = 'cadastro'),
     path('home/', views.home, name = 'home'),
+    path('lancar/', views.lancar, name = 'lancar'),
+    path('alterar/', views.alterar, name = 'alterar'),
+    path('visualizar/', views.visualizar, name = 'visualizar'),
 ]
